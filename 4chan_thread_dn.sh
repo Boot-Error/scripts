d4339042a81a693139c5677274417a41b7f3f387
Boot-Error <booterror99@gmail.com>
2020-09-19 20:24:32 +0300

    fix: changes in curl commands

--- 4chan_thread_dn.sh
+++ 4chan_thread_dn.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 BOARD=$1
-THREAD_ID=$2
 
 API_URL=https://a.4cdn.org/$BOARD/thread/$THREAD_ID.json
 
 function get_all_urls() {
 
-	JSON_DATA=curl -s $API_URL
+	JSON_DATA=curl $API_URL
 
 }
